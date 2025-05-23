725fbc34e782acaade9e74034a760004442a0c02
Daniel Wildman <iWildman1@hotmail.com>
2025-05-23 22:02:21 +0100

    replace eslint and prettier with biome

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
+import { CronJob } from "cron";
 import {
-  Client,
-  GatewayIntentBits,
-  ChannelType,
-  type TextChannel,
-} from 'discord.js';
-import { CronJob } from 'cron';
-import { env } from './env';
-import { getEventsList } from './events';
+	ChannelType,
+	Client,
+	GatewayIntentBits,
+	type TextChannel,
+} from "discord.js";
+import { env } from "./env";
+import { getEventsList } from "./events";
 
 const CHUNK_SIZE = 1500;
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+	intents: [GatewayIntentBits.Guilds],
 });
 
 client.login(env.DISCORD_TOKEN);
@@ -20,59 +20,59 @@ let channel: TextChannel;
 
 // Create the scheduled task but don't start it running straight away
 // Runs every 30m between 7am and 10pm - https://crontab.guru/#*/30_7-22_*_*_*
-const job = new CronJob('*/15 7-22 * * *', checkForEventsAndSend, null, false);
+const job = new CronJob("*/15 7-22 * * *", checkForEventsAndSend, null, false);
 
 // Boot up the Discord integration and start the scheduler
-client.on('ready', () => {
-  const internalChannel = client.channels.cache.get(env.DISCORD_CHANNEL);
+client.on("ready", () => {
+	const internalChannel = client.channels.cache.get(env.DISCORD_CHANNEL);
 
-  if (internalChannel?.type !== ChannelType.GuildText) {
-    throw new Error(
-      'The targeted channel must be a text channel. DanBot cannot work with any other kind of channel'
-    );
-  }
+	if (internalChannel?.type !== ChannelType.GuildText) {
+		throw new Error(
+			"The targeted channel must be a text channel. DanBot cannot work with any other kind of channel",
+		);
+	}
 
-  channel = internalChannel;
-  job.start();
+	channel = internalChannel;
+	job.start();
 });
 
 async function checkForEventsAndSend() {
-  console.log(`[DanBot] Starting event scan at ${new Date().toString()}.`);
+	console.log(`[DanBot] Starting event scan at ${new Date().toString()}.`);
 
-  const events = await getEventsList();
+	const events = await getEventsList();
 
-  // Send a full response if we did get a list
-  if (events.length > 0) {
-    sendEventsResponse(events);
-  }
+	// Send a full response if we did get a list
+	if (events.length > 0) {
+		sendEventsResponse(events);
+	}
 
-  console.log(
-    `[DanBot] Successfully ran event scan at ${new Date().toString()}. Detected ${
-      events.length
-    } new events`
-  );
+	console.log(
+		`[DanBot] Successfully ran event scan at ${new Date().toString()}. Detected ${
+			events.length
+		} new events`,
+	);
 }
 
 function sendEventsResponse(events: Awaited<ReturnType<typeof getEventsList>>) {
-  const eventTitles = events.map((event) => `- ${event.name.text}\n`).join('');
+	const eventTitles = events.map((event) => `- ${event.name.text}\n`).join("");
 
-  // Discord has a max message size of 2,000 characters - We need to split our message into
-  // multiple message chunks to stay under this
-  const chunkedTitles: string[] = [];
-  let currentChunk = 0;
+	// Discord has a max message size of 2,000 characters - We need to split our message into
+	// multiple message chunks to stay under this
+	const chunkedTitles: string[] = [];
+	let currentChunk = 0;
 
-  while (currentChunk < eventTitles.length) {
-    chunkedTitles.push(
-      eventTitles.substring(currentChunk, currentChunk + CHUNK_SIZE)
-    );
-    currentChunk += CHUNK_SIZE;
-  }
+	while (currentChunk < eventTitles.length) {
+		chunkedTitles.push(
+			eventTitles.substring(currentChunk, currentChunk + CHUNK_SIZE),
+		);
+		currentChunk += CHUNK_SIZE;
+	}
 
-  channel.send(
-    `<@${env.DISCORD_TAG_ID}>\n**Waterstones Eventbrite search complete.**\nFound ${events.length} events:`
-  );
+	channel.send(
+		`<@${env.DISCORD_TAG_ID}>\n**Waterstones Eventbrite search complete.**\nFound ${events.length} events:`,
+	);
 
-  chunkedTitles?.forEach((chunk) => {
-    if (chunk.length > 0) channel.send(chunk);
-  });
+	for (const chunk of chunkedTitles || []) {
+		if (chunk.length > 0) channel.send(chunk);
+	}
 }
